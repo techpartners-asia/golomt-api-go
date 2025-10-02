ac29fbe096f01105d117a29f527fd3225a52e34c
croak.g <croak.g@gmail.com>
2025-10-02 15:44:01 +0800

    Add String methods for AccountTypeEnum, AccountStatusEnum, and CardMerchantStatementTypeEnum in constants.go for improved type representation in the openbank package.

--- openbank/model/constants.go
+++ openbank/model/constants.go
@@ -8,6 +8,10 @@ const (
 	LoanAccountType AccountTypeEnum = "LOAN" // Зээлийн данс
 )
 
+func (a AccountTypeEnum) String() string {
+	return string(a)
+}
+
 type FreezeStatusCodeEnum string
 
 const (
@@ -23,3 +27,23 @@ const (
 	AccountStatusInactive AccountStatusEnum = "I" // Inactive
 	AccountStatusDormant  AccountStatusEnum = "D" // Dormant
 )
+
+func (a AccountStatusEnum) String() string {
+	return string(a)
+}
+
+type CardMerchantStatementTypeEnum string
+
+const (
+	CardMerchantStatementTypePOSF    CardMerchantStatementTypeEnum = "POSF"    // POSF
+	CardMerchantStatementTypePOSS    CardMerchantStatementTypeEnum = "POSS"    // POSS
+	CardMerchantStatementTypeFAILED  CardMerchantStatementTypeEnum = "FAILED"  // FAILED
+	CardMerchantStatementTypeSUCCESS CardMerchantStatementTypeEnum = "SUCCESS" // SUCCESS
+	CardMerchantStatementTypeTNKSUC  CardMerchantStatementTypeEnum = "TNKSUC"  // TNKSUC
+	CardMerchantStatementTypeTNKFAL  CardMerchantStatementTypeEnum = "TNKFAL"  // TNKFAL
+	CardMerchantStatementTypeSTTL    CardMerchantStatementTypeEnum = "STTL"    // STTL
+)
+
+func (c CardMerchantStatementTypeEnum) String() string {
+	return string(c)
+}
